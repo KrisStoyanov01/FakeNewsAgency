61d3a6f3ecc3d0cc00e9e987a94bc2f27ff050d8
KRIS0123 <kris12112001@gmail.com>
2019-04-07 19:36:17 +0300

    First commit

---
+++ src/main/java/fakenewsagency/FakenewsagencyApplication.java
@@ -0,0 +1,13 @@
+package fakenewsagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakenewsagencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakenewsagencyApplication.class, args);
+    }
+
+}
